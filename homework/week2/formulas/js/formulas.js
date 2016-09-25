e44b2ce1140e082930c94cb2b34918171ab20fde
ashwath22 <ashwath.anand.22@gmail.com>
2016-09-24 22:53:42 -0700

    Add homework folder to jsd4 repo

---
+++ homework/week2/formulas/js/formulas.js
@@ -0,0 +1,22 @@
+
+// Instructions for your homework
+// //////////////////////////////////////////////////////////////////
+// 1.   Here is where your functions should be defined
+// 2.	 What should you name your functions?  Hint:  check the console to see which functions are already being called.  Are they all "defined?"  If not yet defined... then define them here!
+// 3.	 Be sure to link up this file in your HTML doc
+/////////////////////////////////////////////////////////////////////
+
+function calcLongestSide(a,b){
+	var c= Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+	console.log(c);
+	return c;
+}
+
+function calcCircumference(r){
+	var circumference= 2*Math.PI*r;
+	return circumference;
+}
+
+function calcFahrenheitToCelcius(f){
+	var c= 
+}
